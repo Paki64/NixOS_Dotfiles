2b8d40e20d7ad5a6d993cfe88021cd5d8b8978b5
Paki64 <pasqualecriscuolo2010@gmail.com>
2025-03-23 22:52:05 +0100

    Configuring Rclone

--- hosts/server/services/rclone.nix
+++ hosts/server/services/rclone.nix
@@ -1,68 +1,31 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-
   environment.systemPackages = [ pkgs.rclone ];
-
-/*
-  fileSystems."/home/paki/mnt/server" = {
-    device = "Server:";
-    fsType = "rclone";
-    options = [
-      "nodev"
-      "nofail"
-      "allow_other"
-      "args2env"
-      "config=/home/paki/nixos/dots/common/rclone/rclone.conf"
-      "vfs_cache_mode=full"
-      "vfs_cache_max_size=300G"
-      "vfs_cache_max_age=168h"
-      "buffer_size=1G"
-      "dir_cache_time=168h"
-      "vfs_read_chunk_size=128M"
-      "attr_timeout=5000h"
-      "poll_interval=30s"
-      #"no_checksums"
-      #"no_modtime"
-    ];
+  environment.etc."fuse.conf".text = ''
+    user_allow_other
+  '';
+  security.wrappers = {
+    fusermount.source  = "${pkgs.fuse}/bin/fusermount";
   };
 
   systemd.services.rclone-server = {
-    description = "Rclone mount per Server:";
+    description = "Rclone mount for Onedrive Server";
+    enable = true;
     after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      Type = "simple";
-      ExecStart = ''
-        ${pkgs.rclone}/bin/rclone mount Server:/home/paki/mnt/server \
-          --config=/home/paki/nixos/dots/common/rclone/rclone.conf \
-          --vfs-cache-mode full \
-          --vfs-cache-max-size 300G \
-          --vfs-cache-max-age 168h \
-          --buffer-size 1G \
-          --dir-cache-time 168h \
-          --vfs-read-chunk-size 128M \
-          --attr-timeout 5000h \
-          --poll-interval 30s
-      '';
-      ExecStop = "/run/wrappers/bin/fusermount -u /home/paki/mnt/server";
-      Restart = "always";
+      Type = "notify";
+      ExecStart = "${pkgs.rclone}/bin/rclone mount Server: /home/paki/mnt/server --config=home/paki/nixos/dots/common/rclone/rclone.conf --allow-other --vfs-cache-mode full --vfs-cache-max-size 300G --vfs-read-ahead 2G --vfs-cache-max-age 168h --buffer-size 64M --cache-dir /home/paki/tmp/rclone/cache --checkers 1 --tpslimit 3 --transfers 1 --bwlimit-file 40M:40M --low-level-retries 1 --onedrive-no-versions --onedrive-chunk-size 250M --dir-cache-time 168h --vfs-read-chunk-size 128M --vfs-read-chunk-size-limit off --vfs-fast-fingerprint --attr-timeout 5000h --poll-interval 30s --timeout=1m";
+      ExecStop="/bin/fusermount -u %h/mnt/server";
+      Restart = "on-failure";
       RestartSec = "10s";
+      StandardOutput = "journal";
+      StandardError = "journal";
+      Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
     };
-    wantedBy = [ "multi-user.target" ];
-  };
+  };          
 
-*/
-
-  systemd.user.services.rclone-server = {
-    enable = true;
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    description = "Rclone mount for Onedrive Server";
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = ''${pkgs.rclone}/bin/rclone mount Server: /home/paki/mnt/server --config=/home/paki/nixos/dots/common/rclone/rclone.conf'';
-    };
-  };
 
 }
