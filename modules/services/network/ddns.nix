a101320422a388ee78c119fa33795a9c0b3f96d8
Paki64 <pasqualecriscuolo2010@gmail.com>
2025-03-29 13:49:28 +0000

    Replaced DDNS service with ddclient

--- modules/services/network/ddns.nix
+++ modules/services/network/ddns.nix
@@ -1,23 +1,20 @@
 { config, lib, pkgs, ... }:
 
 {
+  
   options = {
     modules.services.network.ddns.enable = 
       lib.mkEnableOption "enables ddns auto-updater";
   };
 
-  # Config file in secrets/dots/ddns.sops
-
+  # Config file in secrets/dots/ddclient.sops
   config = lib.mkIf config.modules.services.network.ddns.enable {
-
     systemd.tmpfiles.rules = [
     ];
     services.ddclient = {
       enable = true;
       configFile = "/etc/ddclient.conf";
     };
-
-
   };
 
 }
