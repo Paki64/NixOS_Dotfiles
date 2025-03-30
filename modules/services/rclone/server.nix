55ee9d8025e8bd3c7eb6f58cde0fe102b793ca7d
Paki64 <pasqualecriscuolo2010@gmail.com>
2025-03-30 16:30:04 +0000

    Optimized rclone mount

--- modules/services/rclone/server.nix
+++ modules/services/rclone/server.nix
@@ -20,11 +20,11 @@ in {
       after = [ "network-online.target" ];
       wants = [ "network-online.target" ];
       wantedBy = [ "multi-user.target" ];
-      restartIfChanged = false;
+      restartIfChanged = true;
       serviceConfig = {
         Type = "notify";
-        ExecPreStart = "/bin/fusermount -u /mnt/rclone";
-        ExecStart = "${pkgs.rclone}/bin/rclone mount ${mountDrive} ${mountPath} --config=${configFile} --log-level DEBUG --log-file ${logFile} --buffer-size 32M --no-check-certificate --allow-other --vfs-cache-mode full --vfs-cache-max-size 100G --vfs-read-chunk-size 256M --vfs-read-ahead 2G --vfs-cache-max-age 168h --cache-dir ${cacheDir} --checkers 1 --tpslimit 3 --transfers 1 --bwlimit-file 40M:40M --low-level-retries 1 --onedrive-no-versions --onedrive-chunk-size 250M --dir-cache-time 168h --vfs-read-chunk-size 128M --vfs-read-chunk-size-limit off --vfs-fast-fingerprint --attr-timeout 5000h --poll-interval 1m --ignore-size --timeout=1m";
+        ExecPreStart = "/bin/fusermount -u /mnt/rclone && rm -rf ${cacheDir}";
+        ExecStart = "${pkgs.rclone}/bin/rclone mount ${mountDrive} ${mountPath} --config=${configFile} --log-level DEBUG --log-file ${logFile} --buffer-size 32M --no-check-certificate --allow-other --vfs-cache-mode full --vfs-cache-max-size 100G --vfs-read-chunk-size 128M --vfs-read-ahead 2G --vfs-cache-max-age 168h --cache-dir ${cacheDir} --onedrive-no-versions --onedrive-chunk-size 250M --dir-cache-time 168h --vfs-read-chunk-size-limit off --vfs-fast-fingerprint --attr-timeout 5000h --poll-interval 1m --ignore-size --timeout=5m";
         ExecStop = "/bin/fusermount -u /mnt/rclone";
         Restart = "on-failure";
         RestartSec = "10s";
