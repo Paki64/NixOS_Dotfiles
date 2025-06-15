f8be64bbcaf30e22f216632e3800f56ed5828387
Paki64 <pasqualecriscuolo2010@gmail.com>
2025-06-16 01:05:53 +0200

    Enabled nextcloud

--- modules/services/nextcloud.nix
+++ modules/services/nextcloud.nix
@@ -7,31 +7,40 @@
   };
 
   config = lib.mkIf config.modules.services.nextcloud.enable {
-    
-      services.nextcloud = {
-        enable = true;
-        hostName = "server"; 
-        package = pkgs.nextcloud31;
-        config.adminpassFile = "/etc/nextcloud-admin-pass";
-        config.dbtype = "sqlite";     
-        settings.trusted_domains = [ "server" "192.168.1.101" ]; 
-        settings.enabledPreviewProviders = [
-          "OC\\Preview\\BMP"
-          "OC\\Preview\\GIF"
-          "OC\\Preview\\JPEG"
-          "OC\\Preview\\Krita"
-          "OC\\Preview\\MarkDown"
-          "OC\\Preview\\MP3"
-          "OC\\Preview\\OpenDocument"
-          "OC\\Preview\\PNG"
-          "OC\\Preview\\TXT"
-          "OC\\Preview\\XBitmap"
-          "OC\\Preview\\HEIC"
-        ];
-      };
 
-      services.nginx.virtualHosts."server".listen = [ { addr = "0.0.0.0"; port = 9880; } ];
-  
+    services.nextcloud = {
+      enable = true;
+      hostName = "server"; 
+      package = pkgs.nextcloud31;
+      config.adminpassFile = "/etc/nextcloud-admin-pass";
+      config.dbtype = "sqlite";     
+      extraApps = {
+        inherit (config.services.nextcloud.package.packages.apps) news contacts calendar tasks;
+      };
+      extraAppsEnable = true;
+      autoUpdateApps.enable = true;
+      database.createLocally = true;
+      settings.trusted_domains = [ 
+        "cloud.pakisrv.com"
+        "server"
+      ]; 
+      settings.enabledPreviewProviders = [
+        "OC\\Preview\\BMP"
+        "OC\\Preview\\GIF"
+        "OC\\Preview\\JPEG"
+        "OC\\Preview\\Krita"
+        "OC\\Preview\\MarkDown"
+        "OC\\Preview\\MP3"
+        "OC\\Preview\\OpenDocument"
+        "OC\\Preview\\PNG"
+        "OC\\Preview\\TXT"
+        "OC\\Preview\\XBitmap"
+        "OC\\Preview\\HEIC"
+      ];
     };
 
-  }
+    services.nginx.virtualHosts."server".listen = [ { addr = "0.0.0.0"; port = 9880; } ];
+
+  };
+
+}
